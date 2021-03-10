d7d1024e960b6eadf640e94b2f5935e4f0fc8221
htset <haris.tsetsekas@gmail.com>
2021-03-11 01:15:01 +0200

    Login tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,18 +1,58 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { User } from '@app/_models/user';
+import { MockActivatedRoute } from '@app/_services/active.route.mock';
+import { of } from 'rxjs';
 
 import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
+let testUser:User = 
+{
+    id: 1,
+    username: 'htset',
+    password: '',
+    firstName: 'Haris',
+    lastName: 'Tse',
+    token: '',
+    role: 'admin',
+    email: 'haris@tse.gr'
+};
+
+fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let activatedRouteStub: MockActivatedRoute;
 
   beforeEach(async () => {
+    activatedRouteStub = new MockActivatedRoute();
+    activatedRouteStub.testParams = {
+      returnUrl: '/checkout'
+    };
+
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule]
+      imports: [
+        ReactiveFormsModule, 
+        RouterTestingModule, 
+        HttpClientTestingModule
+      ],
+/*      
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteStub }
+      ]
+*/
+
+      providers: [ 
+        { 
+          provide: ActivatedRoute,
+          useValue: {snapshot: {queryParams: {'returnUrl': '/checkout'}}}
+        }
+      ]
+      
     })
     .compileComponents();
   });
@@ -26,4 +66,39 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('should process login successfully', fakeAsync(() => {
+    spyOn(component.authenticationService, 'login').and.returnValue(of(testUser));
+    spyOn(component.router, 'navigate');
+
+    tick();
+    fixture.detectChanges();
+
+    let usr: HTMLInputElement = fixture.debugElement.query(By.css('input[formControlName="username"]')).nativeElement
+    usr.value = 'aa';
+    usr.dispatchEvent(new Event('input'));
+    let pass: HTMLInputElement = fixture.debugElement.query(By.css('input[formControlName="password"]')).nativeElement
+    pass.value = 'aa';
+    pass.dispatchEvent(new Event('input'));
+
+    tick();
+    fixture.detectChanges();
+
+    let el: HTMLButtonElement = fixture.debugElement.query(By.css('#login')).nativeElement;
+    expect(el.disabled).toBeFalse();
+    /*
+      https://stackoverflow.com/questions/47387325/angular-unit-test-failing-when-using-submit
+
+      "Because you have no event handler on the button. That's why triggerEventHandler can't trigger any handler on the button. 
+      In your case you have to use saveButton.nativeElement.click() because now click event will be bubbled and submit event will be fired
+
+    */
+    el.click();
+    //el.dispatchEvent(new Event('click'));
+    tick();
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.router.navigate).toHaveBeenCalledWith(['/checkout']);
+  }));
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,17 +14,18 @@ export class LoginComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService
+        public route: ActivatedRoute,
+        public router: Router,
+        public authenticationService: AuthenticationService
     ) { 
         // redirect to home if already logged in
 /*        if (this.authenticationService.currentUserValue.id) { 
             this.router.navigate(['/admin']);
         }
 */
-        if(this.route.snapshot.queryParams['role'] == "customer")
+/*        if(this.route.snapshot.queryParams['role'] == "customer")
             this.error = "Access to page is restricted. You should login with admin credentials";
+*/            
     }
 
     ngOnInit() {
@@ -39,7 +40,7 @@ export class LoginComponent implements OnInit {
 
     onSubmit() {
         this.submitted = true;
-
+        console.log('on submit');
         // stop here if form is invalid
         if (this.loginForm?.invalid) {
             return;
@@ -50,8 +51,11 @@ export class LoginComponent implements OnInit {
             .pipe(first())
             .subscribe({
                 next: () => {
+                    console.log('login ok');
+                    console.log(this.route.snapshot);
                     // get return url from route parameters or default to '/'
                     const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    console.log(returnUrl);
                     this.router.navigate([returnUrl]);
                 },
                 error: error => {
